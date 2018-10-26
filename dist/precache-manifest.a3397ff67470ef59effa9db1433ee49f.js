49bc91124dc6d617560e532e05e63f1b7be2b7f3
jv <ljw@live.jp>
2018-10-26 23:15:07 +0800

    build: release

--- dist/precache-manifest.a3397ff67470ef59effa9db1433ee49f.js
+++ dist/precache-manifest.a3397ff67470ef59effa9db1433ee49f.js
@@ -1,58 +1,58 @@
 self.__precacheManifest = [
   {
-    'revision': '371cfbdad02da7744cea',
-    'url': '/css/index.c5e8b177.css'
+    "revision": "371cfbdad02da7744cea",
+    "url": "/css/index.c5e8b177.css"
   },
   {
-    'revision': 'b6216d61c03e6ce0c9aea6ca7808f7ca',
-    'url': '/robots.txt'
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
   },
   {
-    'revision': '371cfbdad02da7744cea',
-    'url': '/js/index.ab187aef.js'
+    "revision": "371cfbdad02da7744cea",
+    "url": "/js/index.ab187aef.js"
   },
   {
-    'revision': '682b7c690103bdcff0f2',
-    'url': '/js/@.42c5bc16.js'
+    "revision": "682b7c690103bdcff0f2",
+    "url": "/js/@.42c5bc16.js"
   },
   {
-    'revision': '9aa0594d3875a22254c2',
-    'url': '/js/error.827295e3.js'
+    "revision": "9aa0594d3875a22254c2",
+    "url": "/js/error.827295e3.js"
   },
   {
-    'revision': '9dd9ed7b5fd0357a799c',
-    'url': '/js/game-snake.7d58a49d.js'
+    "revision": "9dd9ed7b5fd0357a799c",
+    "url": "/js/game-snake.7d58a49d.js"
   },
   {
-    'revision': '5b6ce99ba492db2c56ae',
-    'url': '/js/home.01dd6430.js'
+    "revision": "5b6ce99ba492db2c56ae",
+    "url": "/js/home.01dd6430.js"
   },
   {
-    'revision': '1704981a47c687cf600e',
-    'url': '/js/chunk-2d0cf36c.58dab3fb.js'
+    "revision": "1704981a47c687cf600e",
+    "url": "/js/chunk-2d0cf36c.58dab3fb.js"
   },
   {
-    'revision': 'cdf4d09af0ef6b5bf9db',
-    'url': '/js/chunk-vendors.d775e80d.js'
+    "revision": "cdf4d09af0ef6b5bf9db",
+    "url": "/js/chunk-vendors.d775e80d.js"
   },
   {
-    'revision': '31c4c8328c47586d37a6',
-    'url': '/js/social-at.0a5ba642.js'
+    "revision": "31c4c8328c47586d37a6",
+    "url": "/js/social-at.0a5ba642.js"
   },
   {
-    'revision': '825462e7ea1222ca1cb8',
-    'url': '/js/root.e2ae044b.js'
+    "revision": "825462e7ea1222ca1cb8",
+    "url": "/js/root.e2ae044b.js"
   },
   {
-    'revision': 'ca2efac4a204a894ed8ed7de069aeae0',
-    'url': '/index.html'
+    "revision": "ca2efac4a204a894ed8ed7de069aeae0",
+    "url": "/index.html"
   },
   {
-    'revision': '825462e7ea1222ca1cb8',
-    'url': '/css/root.ba10379c.css'
+    "revision": "825462e7ea1222ca1cb8",
+    "url": "/css/root.ba10379c.css"
   },
   {
-    'revision': 'cdf4d09af0ef6b5bf9db',
-    'url': '/css/chunk-vendors.a61c95c6.css'
+    "revision": "cdf4d09af0ef6b5bf9db",
+    "url": "/css/chunk-vendors.a61c95c6.css"
   }
-]
+];
