341cba55e30154d5e9adc0fa8d45f20ab18d0609
jv <ljw@live.jp>
2018-10-31 10:22:27 +0800

    feat: release

---
+++ dist/precache-manifest.16118633557c9606db24db818e14b5cf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    'revision': 'e3f0dcd83e909f19c5cc',
+    'url': '/css/index.c5e8b177.css'
+  },
+  {
+    'revision': 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    'url': '/robots.txt'
+  },
+  {
+    'revision': 'e3f0dcd83e909f19c5cc',
+    'url': '/js/index.c3f9d3e2.js'
+  },
+  {
+    'revision': '682b7c690103bdcff0f2',
+    'url': '/js/@.42c5bc16.js'
+  },
+  {
+    'revision': '9aa0594d3875a22254c2',
+    'url': '/js/error.827295e3.js'
+  },
+  {
+    'revision': '0c3902ad7df0ff941186',
+    'url': '/js/game-snake.e68baa01.js'
+  },
+  {
+    'revision': '5b6ce99ba492db2c56ae',
+    'url': '/js/home.01dd6430.js'
+  },
+  {
+    'revision': '1704981a47c687cf600e',
+    'url': '/js/chunk-2d0cf36c.58dab3fb.js'
+  },
+  {
+    'revision': 'cdf4d09af0ef6b5bf9db',
+    'url': '/js/chunk-vendors.d775e80d.js'
+  },
+  {
+    'revision': '31c4c8328c47586d37a6',
+    'url': '/js/social-at.0a5ba642.js'
+  },
+  {
+    'revision': '825462e7ea1222ca1cb8',
+    'url': '/js/root.e2ae044b.js'
+  },
+  {
+    'revision': '0d364e2d2e7118037a9d9a7d6c4b21ce',
+    'url': '/index.html'
+  },
+  {
+    'revision': '825462e7ea1222ca1cb8',
+    'url': '/css/root.ba10379c.css'
+  },
+  {
+    'revision': 'cdf4d09af0ef6b5bf9db',
+    'url': '/css/chunk-vendors.a61c95c6.css'
+  }
+]
