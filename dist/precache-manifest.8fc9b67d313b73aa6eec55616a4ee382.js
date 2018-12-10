b0714545d0d9818005f35448097372daa332dc05
jv <ljw@live.jp>
2018-12-10 16:01:06 +0800

    fix: 🔥 preload & prefetch

---
+++ dist/precache-manifest.8fc9b67d313b73aa6eec55616a4ee382.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a08f64730f96b38085c",
+    "url": "/css/index.c5e8b177.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0a08f64730f96b38085c",
+    "url": "/js/index.76d068fd.js"
+  },
+  {
+    "revision": "a3c9a1807c490d266610",
+    "url": "/js/@.cf191a07.js"
+  },
+  {
+    "revision": "d8412f40eac3aba85d35",
+    "url": "/js/error.3b2bc486.js"
+  },
+  {
+    "revision": "b7bf9c3ebd36df03bbe3",
+    "url": "/js/game-snake.597dd0e1.js"
+  },
+  {
+    "revision": "2d95f34355dcdc087703",
+    "url": "/js/home.30c4d7d9.js"
+  },
+  {
+    "revision": "bfa39358f7edc3e158b2",
+    "url": "/js/chunk-2d0cf36c.06132996.js"
+  },
+  {
+    "revision": "d83ea04629560998f29e",
+    "url": "/js/chunk-vendors.f29b9a0c.js"
+  },
+  {
+    "revision": "9774991f8401f40537eb",
+    "url": "/js/social-at.8182da59.js"
+  },
+  {
+    "revision": "88633fa265e28b1c6f10",
+    "url": "/js/root.d3ac718d.js"
+  },
+  {
+    "revision": "ac51062ed70b6579e10c712a42747333",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88633fa265e28b1c6f10",
+    "url": "/css/root.25e2b2e4.css"
+  },
+  {
+    "revision": "d83ea04629560998f29e",
+    "url": "/css/chunk-vendors.a61c95c6.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ed5ef4c1e4b06a073820ed862540821.js"
+  "/precache-manifest.8fc9b67d313b73aa6eec55616a4ee382.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "app"});
