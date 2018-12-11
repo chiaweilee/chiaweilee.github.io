c8cbfe6c3353cc2154650f9778ed92b253254e78
jv <ljw@live.jp>
2018-12-11 10:09:01 +0800

    fix: 🔥 remove a2hs

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import Vue from './vue'
 import r from './router'
 import store from './store'
-import addToHomeScreen from './addToHomeScreen'
 import './registerServiceWorker'
 
 require('fastclick').attach(document.body)
-
-addToHomeScreen()
-  .then(prompt => prompt())
 
 new Vue({
   router: r(store),
