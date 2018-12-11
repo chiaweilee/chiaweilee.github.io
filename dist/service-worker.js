ff8966bff77fa09890ecdfd7800aa0c9bed4daa7
jv <ljw@live.jp>
2018-12-11 10:19:08 +0800

    Revert "build: 🔥 release"

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,19 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js')
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  '/precache-manifest.6e686f42a9dfdd59f281cbc495f3129b.js'
-)
+  "/precache-manifest.6e686f42a9dfdd59f281cbc495f3129b.js"
+);
 
-workbox.core.setCacheNameDetails({ prefix: 'app' })
+workbox.core.setCacheNameDetails({prefix: "app"});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.suppressWarnings()
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
