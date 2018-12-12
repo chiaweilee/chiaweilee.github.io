32b320986499bf278a5648c8121d0d7bab9f3ee7
jv <ljw@live.jp>
2018-12-12 15:05:23 +0800

    fix: 🔥 typo

--- scripts/git-guard.js
+++ scripts/git-guard.js
@@ -3,7 +3,7 @@ const createHandler = require('github-webhook-handler')
 const handler = createHandler({ path: '/', secret: '7d38cdd689735b008b3c702edd92eea23791c5f6' })
 
 handler.on('error', function (err) {
-  process.stderr.output(err)
+  process.stderr.write(err)
 })
 
 handler.on('push', function () {
