cda0ef63733886c6ae31f087fed369e8be9976c9
jv <ljw@live.jp>
2019-02-11 20:07:08 +0800

    refactor: 🔥 banner

--- chainWebpack.js
+++ chainWebpack.js
@@ -1,6 +1,7 @@
 
 module.exports = function (config, { webpack }) {
   config.merge({
+    uglifyOptions: {},
     plugin: {
       'banner-js': {
         plugin: require('banner-js-webpack-plugin'),
@@ -8,17 +9,6 @@ module.exports = function (config, { webpack }) {
           require('./scripts/banner')
         ]
       },
-      'banner': {
-        plugin: webpack.BannerPlugin,
-        args: [
-          {
-            entryOnly: true,
-            banner: `${process.env.npm_package_name} v${process.env.npm_package_version}\n` +
-                '(c) 2019 +v\n' +
-                'Released under the MIT License.'
-          }
-        ]
-      }
     }
   });
 }
