c027786428816c4eda7bac3ae6aa5ad9805c20dc
jv <ljw@live.jp>
2019-02-24 14:05:06 +0800

    feat: antd-m

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Dispatch, Store } from 'redux'
+import { Dispatch, Store } from 'redux';
 import { withRouter } from 'react-router-dom';
+import { Flex, WhiteSpace } from 'antd-mobile';
 
 interface Props extends React.Props<any> {
   store?: Store;
@@ -10,10 +11,31 @@ interface Props extends React.Props<any> {
   location?: Location;
 }
 
+const PlaceHolder = props => (
+  <div
+    style={{
+      backgroundColor: '#ebebef',
+      color: '#bbb',
+      textAlign: 'center',
+      height: '30px',
+      lineHeight: '30px',
+      width: '100%',
+    }}
+  >
+    hello, {JSON.stringify(props.store)}!
+  </div>
+);
+
 class Index extends PureComponent<Props> {
   render() {
     const { children, store } = this.props;
-    return <div>hello, {JSON.stringify(store)}!</div>;
+    return (
+      <Flex>
+        <Flex.Item>
+          <PlaceHolder store={store} />
+        </Flex.Item>
+      </Flex>
+    );
   }
 }
 
