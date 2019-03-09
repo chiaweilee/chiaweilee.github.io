9adb490aaf98003a1bc3114cd8cdb84aaa21fdef
jv <ljw@live.jp>
2019-03-09 11:25:01 +0800

    feat: analyzer

--- chainWebpack.js
+++ chainWebpack.js
@@ -1,6 +1,10 @@
 module.exports = function(config /* , { webpack } */) {
   config.merge({
     plugin: {
+      analyzer: {
+        plugin: require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
+        args: [],
+      },
       'banner-js': {
         plugin: require('banner-js-webpack-plugin'),
         args: [require('./src/banner')],
