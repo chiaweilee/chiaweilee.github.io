66dfb449aa365a26afee495399a91804a3ac3cce
jv <ljw@live.jp>
2019-03-09 15:10:49 +0800

    feat: why

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import dva from 'dva';
 import createHistory from 'history/createBrowserHistory';
 
+if (process.env.NODE_ENV === 'develop') {
+  require('why-did-you-update').whyDidYouUpdate(React);
+}
+
 const app = dva({
   history: createHistory(),
   initialState: {},
