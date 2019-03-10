f1fc92cd4ca07d9b043d463f40fc5e2060af2506
jv <ljw@live.jp>
2019-03-10 17:16:43 +0800

    test: fix

--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,14 +1,34 @@
 import 'jest';
 import Index from '..';
 import React from 'react';
-import renderer, { ReactTestInstance, ReactTestRenderer } from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import renderer, { ReactTestRenderer } from 'react-test-renderer';
+
+const store = createStore(function(state = []) {
+  return state;
+});
+
+const Router = require('dva/router').routerRedux.ConnectedRouter;
+
+const g_history = createHistory({
+  basename: '/',
+});
 
 describe('Page: index', () => {
   it('Render correctly', () => {
-    const wrapper: ReactTestRenderer = renderer.create(<Index />);
-    expect(wrapper.root.children.length).toBe(1);
-    const outerLayer = wrapper.root.children[0] as ReactTestInstance;
-    expect(outerLayer.type).toBe('div');
+    const testRenderer: ReactTestRenderer = renderer.create(
+      <Provider store={store}>
+        <Router history={g_history}>
+          <Index />
+        </Router>
+      </Provider>
+    );
+
+    const testInstance = testRenderer.root;
+
+    const outerLayer = testInstance.findByType(Index);
     expect(outerLayer.children.length).toBe(1);
   });
 });
