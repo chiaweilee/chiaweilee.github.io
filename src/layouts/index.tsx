f082d328f958eb51a88400457eac2b852b592d95
jv <ljw@live.jp>
2019-03-10 20:05:19 +0800

    feat: transition

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { NavBar, Icon } from 'antd-mobile';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export type BasicLayoutComponent<Props> = React.SFC<Props>;
 
@@ -7,11 +10,31 @@ export interface BasicLayoutProps extends React.Props<any> {
   location?: Location;
 }
 
-const BasicLayout: BasicLayoutComponent<BasicLayoutProps> = ({ children }) => {
+const NavBarRightContent: Array = [
+  (
+    <Link key={'test'} to={'/test'}>
+      <Icon key="1" type="ellipsis" />
+    </Link>
+  ),
+];
+
+const BasicLayout: BasicLayoutComponent<BasicLayoutProps> = ({ children, history, location }) => {
   return (
-    <div>
-      {children}
-    </div>
+    <TransitionGroup>
+      <CSSTransition key={location.pathname} classNames="transition" timeout={300}>
+        <div className={'app'}>
+          <NavBar
+            mode="light"
+            icon={<Icon type="left" />}
+            onLeftClick={history.goBack}
+            rightContent={NavBarRightContent}
+          >
+            {JSON.stringify(location.pathname)}
+          </NavBar>
+          {children}
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
