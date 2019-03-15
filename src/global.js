04d854031fa768105273fb04e72568dfd9e37614
jv <ljw@live.jp>
2019-03-15 23:30:30 +0800

    feat: notification

--- src/global.js
+++ src/global.js
@@ -1,5 +1,22 @@
 import device from 'current-device';
 import { config as configIconfont } from 'iconfont-symbol';
+import Notify from 'notifyjs';
+
+function notification() {
+  const note = new Notify('Dear red', {
+    body: 'Hope is a good thing, maybe the best of things, and no good thing ever dies.',
+  });
+
+  note.show();
+}
+
+if (!Notify.needsPermission) {
+  notification();
+} else {
+  Notify.requestPermission(() => {
+    notification();
+  });
+}
 
 configIconfont('1089854_phq99uk0x5');
 
