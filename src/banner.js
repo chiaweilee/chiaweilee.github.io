abbe14ae905936b990dfc11ff67ca9986b7e584a
jv <ljw@live.jp>
2019-03-21 08:09:23 +0800

    feat: vconsole

--- src/banner.js
+++ src/banner.js
@@ -1,3 +1,10 @@
 module.exports = /* !!( */ function() {
   // banner-js
+  const script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/vconsole';
+  document.body.appendChild(script);
+  // eslint-disable-next-line
+  script.onload = script.onerror = () => {
+    document.body.removeChild(script);
+  };
 }; /* )(); */
