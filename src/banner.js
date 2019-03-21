c0186e02757f13851af2a8b1cb3c24950dc3d219
jv <ljw@live.jp>
2019-03-21 08:19:06 +0800

    feat: vconsole

--- src/banner.js
+++ src/banner.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-new,no-undef,no-multi-assign */
+
 module.exports = /* !!( */ function() {
   // banner-js
   const script = document.createElement('script');
   script.src = 'https://cdn.jsdelivr.net/npm/vconsole';
   document.body.appendChild(script);
-  // eslint-disable-next-line
   script.onload = script.onerror = () => {
+    new VConsole();
     document.body.removeChild(script);
   };
 }; /* )(); */
