d056d776cd354ea3ac5492c3a6b5a30959f59562
jv <ljw@live.jp>
2019-03-23 20:49:20 +0800

    fix: test

--- src/layouts/__tests__/index.test.tsx
+++ src/layouts/__tests__/index.test.tsx
@@ -3,11 +3,16 @@ import BasicLayout from '..';
 import React from 'react';
 import renderer, { ReactTestInstance, ReactTestRenderer } from 'react-test-renderer';
 
+const location = { pathname: '/' };
+const history = { goBack: new Function() };
+
 describe('Layout: BasicLayout', () => {
   it('Render correctly', () => {
-    const wrapper: ReactTestRenderer = renderer.create(<BasicLayout />);
+    const wrapper: ReactTestRenderer = renderer.create(
+      <BasicLayout location={location} history={history} />
+    );
     expect(wrapper.root.children.length).toBe(1);
     const outerLayer = wrapper.root.children[0] as ReactTestInstance;
-    expect(outerLayer.type).toBe('div');
+    expect(typeof outerLayer.type).toBe('function');
   });
 });
