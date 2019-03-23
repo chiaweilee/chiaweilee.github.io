20c5205181a797598f04c239e7eb45ccf4a5fd64
jv <ljw@live.jp>
2019-03-23 15:58:39 +0800

    build: release

---
+++ dist/precache-manifest.ff3e72de585bf3ea6c732f9e8d4dbcf0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "09cacff3b405a1f911ae",
+    "url": "/umi.fe9034da.css"
+  },
+  {
+    "revision": "e8d4db0b04f8b0aaf366",
+    "url": "/vendors.a0f5595e.chunk.css"
+  },
+  {
+    "revision": "e8d4db0b04f8b0aaf366",
+    "url": "/vendors.a99f9794.async.js"
+  },
+  {
+    "revision": "09cacff3b405a1f911ae",
+    "url": "/umi.04b3c06f.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/.gitkeep"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "d4b157966ed86ac49ebd",
+    "url": "/p__test.58bd628a.async.js"
+  },
+  {
+    "revision": "652141ca6a2257ab8dfc",
+    "url": "/p__index.85416388.async.js"
+  },
+  {
+    "revision": "7ae002b3efdb66bb4040591fb0fb1183",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "46bfe3ed3919192ce61b",
+    "url": "/layouts__index.ff2967de.async.js"
+  },
+  {
+    "revision": "46bfe3ed3919192ce61b",
+    "url": "/layouts__index.a2dada3d.chunk.css"
+  },
+  {
+    "revision": "df380c587206861109e968487369c58b",
+    "url": "/index.html"
+  }
+];
--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,3 @@
-importScripts("/precache-manifest.4380bead734e641d9d60b9234f3694e6.js", "/workbox-v3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.ff3e72de585bf3ea6c732f9e8d4dbcf0.js", "/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
