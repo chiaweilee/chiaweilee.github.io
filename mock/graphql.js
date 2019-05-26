1d33bd146614b3611a990efe994d9ca49d35a3bd
jv <ljw@live.jp>
2019-05-26 15:54:48 +0800

    refactor: gge

--- mock/graphql.js
+++
@@ -1,6 +0,0 @@
-const gql = require('../server/gql-response');
-const type = require('../server/type.graphql');
-
-export default {
-  'GET /q': gql(type),
-};
--- server/ssr.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(req, res, next) {
-  // TODO https://github.com/umijs/umi/issues/1330
-  next();
-};
