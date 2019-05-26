bbaa67ee75b46f031a310fcc6a7c0f102d405abe
+v <ljw@live.jp>
2019-05-26 15:57:53 +0800

    refactor: gge (#13)

--- server/gql-response.js
+++
@@ -1,7 +0,0 @@
-const { graphql } = require('graphql');
-
-module.exports = ({ schema, root }) => ({ url }, res) => {
-  graphql(schema, url.split('?')[1], root).then(result => {
-    res.send(result);
-  });
-};
