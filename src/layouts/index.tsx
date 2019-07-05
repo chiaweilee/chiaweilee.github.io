5060e523c112dc6972b72efcc45e534921346052
jv <ljw@live.jp>
2019-07-05 23:52:01 +0800

    feat: md

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 interface Props {
   children: any;
+  location: Location;
 }
 
 export default (props: Props) => {
+  if (/^docs\//.test(props.location.pathname)) {
+    return <div><h3>docs</h3>{props.children}</div>;
+  }
+
   return props.children;
 };
