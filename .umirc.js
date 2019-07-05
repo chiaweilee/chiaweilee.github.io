8a2d3a34bfc57553230a51f9be00657f87825b3d
jv <ljw@live.jp>
2019-07-05 23:31:19 +0800

    feat: history

--- .umirc.js
+++ .umirc.js
@@ -1,7 +1,7 @@
 // ref: https://umijs.org/config/
 export default {
   hash: true,
-  history: 'hash',
+  history: 'browser',
   treeShaking: true,
   ignoreMomentLocale: true,
   alias: require('./webpack.config').resolve.alias,
