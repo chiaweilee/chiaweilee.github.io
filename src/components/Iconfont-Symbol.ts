9a8c449db6a8828a9d553c7c19a9c3a4a3c17f62
jv <ljw@live.jp>
2019-07-07 10:09:31 +0800

    feat: loader

--- src/components/Iconfont-Symbol.ts
+++ src/components/Iconfont-Symbol.ts
@@ -1,4 +1,4 @@
 import React from 'react';
 import { createIconfontSymbol } from 'iconfont-symbol';
 
-export default createIconfontSymbol('1280925_u93mvrg1soa', {});
+export default createIconfontSymbol('1280925_y8pw1kmus4g', {});
