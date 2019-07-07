93d5247c9261ae5b790783bed0d9194ecf883a05
jv <ljw@live.jp>
2019-07-07 15:24:00 +0800

    build: dist

--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,3 @@
-importScripts("/precache-manifest.faa5755eeb6c47ad3f74ad770dca6c53.js", "/workbox-v3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.9199a929ddda9de4a6b14c8c85253999.js", "/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
