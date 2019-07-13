71008342abb133a74b5cbb5971d9b0cf4136e7ec
jv <ljw@live.jp>
2019-07-13 16:23:49 +0800

    build: release

---
+++ dist/precache-manifest.a21af4c931460b297a7c985e71e30096.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5eba270eb4c79c9984a",
+    "url": "/vendors.b9b95b72.chunk.css"
+  },
+  {
+    "revision": "c5eba270eb4c79c9984a",
+    "url": "/vendors.266b60ad.async.js"
+  },
+  {
+    "revision": "7a99a54167ba47c96f36",
+    "url": "/umi.f16bda74.js"
+  },
+  {
+    "revision": "7a99a54167ba47c96f36",
+    "url": "/umi.0580c0d9.css"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "e346ffcbbdceba6d7b2611b4e41e0dda",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "8ca60a2e968a4d397c59",
+    "url": "/layouts__index.43619b6f.async.js"
+  },
+  {
+    "revision": "99a0cf99dd06f30c7c5f4e2f548d8bda",
+    "url": "/index.html"
+  }
+];
--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,3 @@
-importScripts("/precache-manifest.6820f2fb4fa6480af4a2873dd5cb4d21.js", "/workbox-v3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.a21af4c931460b297a7c985e71e30096.js", "/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
