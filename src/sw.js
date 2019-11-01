2b4e31c4d4cbdeb4023875e82147220479b68ad7
jv <ljw@live.jp>
2019-11-01 22:43:02 +0800

    feat: workbox

---
+++ src/sw.js
@@ -0,0 +1,5 @@
+/* eslint-env worker */
+/* global workbox */
+
+workbox.routing.registerRoute(/.*\.html$/, workbox.strategies.networkFirst());
+workbox.routing.registerRoute(/.*.(?:js|css)/, workbox.strategies.cacheFirst());
