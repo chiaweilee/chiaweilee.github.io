0813ee2d91418dbe48e9335d659f9ba3070e1c9d
jv <ljw@live.jp>
2020-03-22 18:06:09 +0800

    remove: unused

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,15 +10,4 @@ export default class Utils {
       fn();
     }
   };
-  public static setItem = (key: string, value: any) => {
-    localStorage.setItem(key, JSON.stringify(value));
-  };
-  public static getItem = (key: string): any => {
-    try {
-      return JSON.parse(localStorage.getItem(key));
-    } catch (e) {
-      return;
-    }
-  };
-  public static replaceParam = (url: string, param: string) => url.replace('{$}', param);
 }
