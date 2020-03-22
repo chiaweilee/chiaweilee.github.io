c63a95a21225a16cdc03496090d835a1a38b34a6
jv <ljw@live.jp>
2020-03-22 18:02:39 +0800

    remove: dist

--- dist/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("/precache-manifest.234ff83679f0eb1959667348e6df5e2b.js", "/workbox-v3.6.3/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
-/* eslint-env worker */
-/* global workbox */
-
-workbox.routing.registerRoute(/.*\.html$/, workbox.strategies.networkFirst());
-workbox.routing.registerRoute(/.*.(?:js|css)/, workbox.strategies.cacheFirst());
-
