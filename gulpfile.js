17044918c9c10c12f75ddb8c192a4f4861954a2e
jv <ljw@live.jp>
2020-03-25 07:11:17 +0800

    cli: install

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const exec = require('child_process').exec;
 const fetch = 'fetch';
 const reset = 'reset';
 const pull = 'pull';
+const install = 'install';
 const build = 'build';
 
 const createTask = (name, cmd) =>
@@ -16,6 +17,7 @@ const createTask = (name, cmd) =>
 createTask(fetch, 'git fetch --all');
 createTask(reset, 'git reset --hard origin/master');
 createTask(pull, 'git pull');
+createTask(install, 'npm run install');
 createTask(build, 'umi build');
 
-exports.default = series(fetch, reset, pull, build);
+exports.default = series(fetch, reset, pull, install, build);
