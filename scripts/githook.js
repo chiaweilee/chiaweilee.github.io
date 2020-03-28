2a036efa7c10e71467663e4d85ae6df237d651cd
chiaweilee <ljw@live.jp>
2020-03-28 11:29:12 +0800

    script: spawn

--- scripts/githook.js
+++ scripts/githook.js
@@ -1,4 +1,23 @@
-const exec = require('child_process').exec;
+const spawn = (command, ...argv) => {
+  const comanderStr = [command, ...argv].join(' ');
+  process.stdout.write(`starting ${comanderStr}.\n`);
+
+  const ls = require('child_process').spawn(command, [...argv]);
+
+  ls.stdout.on('data', data => {
+    process.stdout.write(data);
+  });
+
+  ls.stderr.on('data', data => {
+    process.stdout.stderr(data);
+  });
+
+  ls.on('close', code => {
+    process.stdout.write(`${comanderStr} finished with code '${code}'.\n`);
+  });
+
+  return ls;
+};
 
 module.exports = function(event) {
   if (event.payload) {
@@ -6,7 +25,9 @@ module.exports = function(event) {
 
     if (ref === 'refs/heads/master') {
       process.stdout.write('master changes detected.\n');
-      exec('gulp');
+      spawn('gulp');
     }
   }
 };
+
+
