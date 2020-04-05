55c666b3494753691e54d77f950f6b1cc6efe35d
chiaweilee <ljw@live.jp>
2020-04-05 19:51:51 +0800

    fix: cr

--- dist/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("/precache-manifest.fa81c8e6c9c0a8ac46cd2f38d02bd5b3.js", "/workbox-v3.6.3/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
-/* eslint-env worker */
-/* global workbox */
-
-workbox.routing.registerRoute(/.*\.html$/, workbox.strategies.networkFirst());
-workbox.routing.registerRoute(/.*.(?:js|css)/, workbox.strategies.cacheFirst());
-
