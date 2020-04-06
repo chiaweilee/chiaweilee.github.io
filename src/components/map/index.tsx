7f6669b8c6c8992a73d8ad0b71e7e4f895957771
chiaweilee <ljw@live.jp>
2020-04-06 12:13:25 +0800

    fix: api-key

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.less';
 const apiKey =
   process.env.NODE_ENV === 'development'
     ? 'AmaJse0LMtAHWktKP2ew2c_NNcKEDFem3a1MWEu8xN0_fNn-alxc7q1BlLEgcQtD'
-    : 'AvHBgtLyf4zbDhXESAuvFMSqIg1GgomX6DnDgw-';
+    : 'AvHBgtLyf4zbDhXESAuvFMSqIg1GgomX6DnDgw-CaXFeRmWVzvXPC55WveE4pJla';
 
 export default (props: any) => {
   const [latitude, longitude] = typeof props.center === 'string' ? props.center.split(',') : [];
