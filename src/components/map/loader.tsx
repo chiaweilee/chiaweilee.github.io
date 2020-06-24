194a1571226509e5e47b15c24eef4b686ad81d71
chiaweilee <ljw@live.jp>
2020-06-24 23:18:43 +0800

    fix: work under multi-thread

--- src/components/map/loader.tsx
+++ src/components/map/loader.tsx
@@ -1,30 +1,34 @@
 export let Microsoft: any;
 declare let window: any;
+let loader: Promise<any>;
 
 export function loadBingApi(key: string): Promise<void> {
   if (!!window.Microsoft) {
     return Promise.resolve();
   }
 
-  const callbackName = 'bingAPIReady';
-  let url = `https://www.bing.com/api/maps/mapcontrol?callback=${callbackName}`;
-  if (key) {
-    url += `&key=${key}`;
+  if (!loader) {
+    loader = new Promise((resolve, reject) => {
+      const callbackName = 'bingAPIReady';
+      let url = `https://www.bing.com/api/maps/mapcontrol?callback=${callbackName}`;
+      if (key) {
+        url += `&key=${key}`;
+      }
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.defer = true;
+      script.src = url;
+      window.bingAPIReady = () => {
+        Microsoft = window.Microsoft;
+        resolve();
+      };
+      script.onerror = (error: any) => {
+        reject(error);
+      };
+      document.body.appendChild(script);
+    });
   }
 
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.defer = true;
-    script.src = url;
-    window.bingAPIReady = () => {
-      Microsoft = window.Microsoft;
-      resolve();
-    };
-    script.onerror = (error: any) => {
-      reject(error);
-    };
-    document.body.appendChild(script);
-  });
+  return loader;
 }
