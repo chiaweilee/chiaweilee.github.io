feb22cb1c6736554bfe981bca4763d145e800093
+v <ljw@live.jp>
2020-11-14 09:34:40 +0800

    Dev (#26)
    * Sync (#24)
    
    * build
    
    * remove: unused
    
    * feat: test
    
    * ci: master only
    
    * build
    
    * ci: master only
    
    * ci: master only
    
    * ci: master only
    
    * ci: master only :(
    
    * ci: master only :(
    
    * ci: master only :(
    
    * ci: master only :(
    
    * ci: master only :(
    
    * feat: navi
    
    * build
    
    * script: spawn
    
    * feat: empty
    
    * build
    
    * docs: types
    
    * build
    
    * remove: targets
    
    * build
    
    * refactor: breaking
    
    * fix: cr
    
    * fix: cr
    
    * fix: cr
    
    * build
    
    * Set theme jekyll-theme-minimal
    
    * build
    
    * fix: router
    
    * refactor: mdx
    
    * build
    
    * Create CNAME
    
    * build
    
    * Create CNAME
    
    * fix: path
    
    * build
    
    * fix: path
    
    * remove: docs
    
    * remove: docs
    
    * remove: unused
    
    * fix: path
    
    * fix: path
    
    * build
    
    * feat: map
    
    * feat: map
    
    * build
    
    * fix: api-key
    
    * feat: map
    
    * feat: map
    
    * docs: thai
    
    * feat: map
    
    * docs: thai
    
    * build
    
    * docs: date
    
    * build
    
    * feat: aerial
    
    * docs: thai
    
    * build
    
    * typo: words
    
    * build
    
    * feat: img
    
    * build
    
    * remove: not working
    
    * build
    
    * remove: unused
    
    * build
    
    * docs: full moon
    
    * build
    
    * docs: thai
    
    * docs: tour
    
    * build
    
    * docs: scuba
    
    * build
    
    * docs: thai
    
    * build
    
    * feat: speak
    
    * build
    
    * typo: doc
    
    * build
    
    * feat: iconfont
    
    * fix: lint
    
    * fix: github edit
    
    * feat: drawer
    
    * build
    
    * fix: empty route
    
    * style: fix
    
    * build
    
    * feat: not ie
    
    * docs: map
    
    * build
    
    * typo: remove
    
    * build
    
    * fix: work under multi-thread
    
    * build
    
    * feat: auto-speak
    
    * build
    
    * fix: cap
    
    * fix: tag
    
    * build
    
    * feat: try using points as center
    
    * docs: update
    
    * build
    
    * feat: setLang
    
    * docs: tour
    
    * build
    
    * feat: img
    
    * build
    
    * doc: english
    
    * build
    
    * Create thai-tour:the-begining
    
    * refactor: nav
    
    * build
    
    * Update ffmpeg.mdx
    
    * build
    
    * docs: TDK
    
    * build
    
    * Update lines.mdx
    
    * build
    
    * T7 (#23)
    
    * Create T1-Bangkok1.md
    
    * Update T1-Bangkok1.md
    
    * Update T1-Bangkok1.md
    
    * docs: t7
    
    * Update T1-Bangkok1.md
    
    * Create T1-Bangkok1.mdx
    
    * Delete T1-Bangkok1.md
    
    * Create T7-KOHPHANGAN1.mdx
    
    * Update config.yml
    
    * Update config.yml
    
    * build
    
    * Update T1-Bangkok1.mdx
    
    * build
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update T1-Bangkok1.mdx
    
    * Update config.yml
    
    * Update config.yml
    
    * Create T1-2-SUKHOTHAI1.md
    
    * Create T1-1-BANGKOK1.mdX
    
    * Delete T1-Bangkok1.mdx
    
    * dep: Cryptorjs
    
    * feat: Cryptor
    
    * feat: Cryptor
    
    * Delete index.tsx
    
    * test: cryptor
    
    * Update config.yml

--- home/sw.js
+++ home/sw.js
@@ -1,3 +1,5 @@
+
+
 importScripts("/home/precache-manifest.7ec24275668292c76343a76098eb2995.js", "/home/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/home/workbox-v3.6.3"});
 /* eslint-env worker */
