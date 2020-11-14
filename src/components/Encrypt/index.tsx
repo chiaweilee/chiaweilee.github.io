5c76e1e9d9333e9ae5250305d8d3d54964097a5c
+v <ljw@live.jp>
2020-11-14 08:48:31 +0800

    Delete index.tsx

--- src/components/Encrypt/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Cryptorjs = require('cryptorjs');
-
-export default class extends React.PureComponents {
-  render () {
-    return new Cryptorjs(window.secretKey).decode(this.props.children);
-  }
-}
