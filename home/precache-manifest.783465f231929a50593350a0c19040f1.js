71deb907c657f56d54cb94f535b505dfe41889ad
chiaweilee <ljw@live.jp>
2020-11-14 01:36:57 +0000

    build

---
+++ home/precache-manifest.783465f231929a50593350a0c19040f1.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b3297d582d58a15581c",
+    "url": "/home/vendors.0e13dc81.chunk.css"
+  },
+  {
+    "revision": "6b3297d582d58a15581c",
+    "url": "/home/vendors.83243a6e.async.js"
+  },
+  {
+    "revision": "17ffeb8134f7646132e1",
+    "url": "/home/p__docs___layout.c587d382.chunk.css"
+  },
+  {
+    "revision": "17ffeb8134f7646132e1",
+    "url": "/home/p__docs___layout.0383de2b.async.js"
+  },
+  {
+    "revision": "68080bac14652799a414",
+    "url": "/home/p__docs__english__pronunciation.mdx.b7e08b15.chunk.css"
+  },
+  {
+    "revision": "68080bac14652799a414",
+    "url": "/home/p__docs__english__pronunciation.mdx.bf624008.async.js"
+  },
+  {
+    "revision": "cb1c19b78a36c6eb4b40",
+    "url": "/home/p__docs__f2e__css__BFC.mdx.48445f3b.async.js"
+  },
+  {
+    "revision": "58d4e957fec1efd95a3c",
+    "url": "/home/p__docs__f2e__css__Box-model.mdx.98b354c4.async.js"
+  },
+  {
+    "revision": "ddb6f73900afc14a5dd6",
+    "url": "/home/p__docs__f2e__css__Horizontal-and-Vertical-Center.mdx.9f08022e.async.js"
+  },
+  {
+    "revision": "418b98d0932a9b001a1d",
+    "url": "/home/p__docs__f2e__css__REM.mdx.171c78f3.async.js"
+  },
+  {
+    "revision": "169d7ac1459ce7421c3f",
+    "url": "/home/p__docs__f2e__http__Headers-and-Caching.mdx.0b414910.async.js"
+  },
+  {
+    "revision": "cf28c403293c0e2f7373",
+    "url": "/home/p__docs__f2e__javascript__AMD-UMD-CommonJS.mdx.1249833c.async.js"
+  },
+  {
+    "revision": "d8881126dc298e4b8cdd",
+    "url": "/home/p__docs__f2e__javascript__Array.mdx.98e924de.async.js"
+  },
+  {
+    "revision": "3d36514f87ae39d3cf05",
+    "url": "/home/p__docs__f2e__javascript__BigInt.mdx.70100a9a.async.js"
+  },
+  {
+    "revision": "f080a9e1f044081ad153",
+    "url": "/home/p__docs__f2e__javascript__Closure.mdx.2be9e5a0.async.js"
+  },
+  {
+    "revision": "fb59a7678490ed67a38b",
+    "url": "/home/p__docs__f2e__javascript__Deep&ShallowCopy.mdx.e4df86df.async.js"
+  },
+  {
+    "revision": "801b8db3d9add3e7d8be",
+    "url": "/home/p__docs__f2e__javascript__Enumerate-an-object.mdx.5e2c70a3.async.js"
+  },
+  {
+    "revision": "2af3a2d0b2c4bba09b81",
+    "url": "/home/p__docs__f2e__javascript__Event.mdx.6fc13453.async.js"
+  },
+  {
+    "revision": "75ff7f897228a7a9567d",
+    "url": "/home/p__docs__f2e__javascript__Microtasks&Macrotasks.mdx.7e26594e.async.js"
+  },
+  {
+    "revision": "a292a53935a5f02235d2",
+    "url": "/home/p__docs__f2e__javascript__Promise.mdx.82853e6d.async.js"
+  },
+  {
+    "revision": "9cefad255ed66e06a9ba",
+    "url": "/home/p__docs__f2e__javascript__Proxy.mdx.6446b90b.async.js"
+  },
+  {
+    "revision": "bb008846af0dfa01c9f9",
+    "url": "/home/p__docs__f2e__javascript__Set.mdx.36133dd3.async.js"
+  },
+  {
+    "revision": "e495b0b04fe35e9cd5d3",
+    "url": "/home/p__docs__f2e__javascript__String.mdx.6cd292f8.async.js"
+  },
+  {
+    "revision": "370e4c380d758c0ff7f3",
+    "url": "/home/p__docs__f2e__javascript__XSS.mdx.d63c45a5.async.js"
+  },
+  {
+    "revision": "94179d2c8c81ff17c571",
+    "url": "/home/p__docs__f2e__javascript__classes.mdx.6ae48009.async.js"
+  },
+  {
+    "revision": "359d98af9fe235999fd5",
+    "url": "/home/p__docs__f2e__javascript__falsy-truthy.mdx.ab59f93d.async.js"
+  },
+  {
+    "revision": "06c96ebe273f9e0f21be",
+    "url": "/home/p__docs__f2e__javascript__memory-leak.mdx.d8452576.async.js"
+  },
+  {
+    "revision": "bbf0fbd1d0a899941fce",
+    "url": "/home/p__docs__f2e__javascript__null.mdx.b95de94e.async.js"
+  },
+  {
+    "revision": "e9413740b25f0a085fb6",
+    "url": "/home/p__docs__f2e__javascript__requestAnimationFrame.mdx.5c50ff9e.async.js"
+  },
+  {
+    "revision": "f451cd0332392afebb85",
+    "url": "/home/p__docs__f2e__javascript__require&import.mdx.05aecc64.async.js"
+  },
+  {
+    "revision": "a0ba910fd0f81a834fcd",
+    "url": "/home/p__docs__f2e__nodejs__npm.mdx.0304b51c.async.js"
+  },
+  {
+    "revision": "6dfe33af6353883cedf1",
+    "url": "/home/p__docs__f2e__nodejs__stdout.mdx.8a041e12.async.js"
+  },
+  {
+    "revision": "a55f3a60c77e9bbedf9c",
+    "url": "/home/p__docs__f2e__pwa__manifest.mdx.bd10aafc.async.js"
+  },
+  {
+    "revision": "f96c94e0435a920f96a6",
+    "url": "/home/p__docs__f2e__vuejs__data-function.mdx.fff227f6.async.js"
+  },
+  {
+    "revision": "de86625f1f08cb5e4252",
+    "url": "/home/p__docs__f2e__vuejs__memory-leak.mdx.e104f26f.async.js"
+  },
+  {
+    "revision": "45662ea2470341d17903",
+    "url": "/home/p__docs__f2e__vuejs__nextTick.mdx.a67b06ab.async.js"
+  },
+  {
+    "revision": "9eca05b7d0a794830ea6",
+    "url": "/home/p__docs__f2e__vuejs__v-if&v-for.mdx.088c9cb1.async.js"
+  },
+  {
+    "revision": "c2d0806b25d889992707",
+    "url": "/home/p__docs__f2e__webpack__Plugins.mdx.47a90a0d.async.js"
+  },
+  {
+    "revision": "c2caf73397702e0c6e62",
+    "url": "/home/p__docs__food__hotpot.mdx.f6e56ff8.async.js"
+  },
+  {
+    "revision": "4698009273530be01cbe",
+    "url": "/home/p__docs__index.mdx.c53702df.async.js"
+  },
+  {
+    "revision": "11e379d013edad7d390a",
+    "url": "/home/p__docs__movie__TDK.mdx.61c3b43f.async.js"
+  },
+  {
+    "revision": "4b6c9158939e7a8409a2",
+    "url": "/home/p__docs__movie__lines.mdx.ddf51acd.async.js"
+  },
+  {
+    "revision": "ffe047ad75a38120c684",
+    "url": "/home/p__docs__t7__T1-2-SUKHOTHAI1.md.9f8d6c87.async.js"
+  },
+  {
+    "revision": "9797bf9f5f3b9837135f",
+    "url": "/home/p__docs__t7__T1-Bangkok1.mdx.0b99d26b.async.js"
+  },
+  {
+    "revision": "00052b33163b59c12b1f",
+    "url": "/home/p__docs__t7__T7-KOHPHANGAN1.mdx.aa86eb9b.async.js"
+  },
+  {
+    "revision": "0c790bec23080cab8538",
+    "url": "/home/p__docs__test__index.mdx.0b7339be.async.js"
+  },
+  {
+    "revision": "a0395c8d9e84d84436d3",
+    "url": "/home/p__docs__tour__1st-thailand-tour:phuket-catch-the-wind.mdx.442cb357.async.js"
+  },
+  {
+    "revision": "3daa63bfb3454f6ff40f",
+    "url": "/home/p__docs__tour__1st-thailand-tour:sukhothai-riding-in-dark.mdx.d5ef73c9.async.js"
+  },
+  {
+    "revision": "59c4bd1c560ca940d67a",
+    "url": "/home/p__docs__tour__1st-thailand-tour:sukhothai-unexpected-ride.mdx.bcc1f1a2.async.js"
+  },
+  {
+    "revision": "9d8f49f42ef87ca463a6",
+    "url": "/home/p__docs__tour__2nd-malaysia:mabul-scuba-paradise.mdx.f91eccc1.async.js"
+  },
+  {
+    "revision": "f2dd08716e49b507672c",
+    "url": "/home/p__docs__tour__checklist.mdx.e5ed8a51.async.js"
+  },
+  {
+    "revision": "19019bf21c382e160d02",
+    "url": "/home/p__docs__tour__emei--20℃.mdx.d63c1549.async.js"
+  },
+  {
+    "revision": "edbbbdb52dd29328e2b0",
+    "url": "/home/p__docs__tour__malaysia-tour:Mount Kinabalu.mdx.26f07748.async.js"
+  },
+  {
+    "revision": "b73a266d6799aa347b38",
+    "url": "/home/p__docs__tour__thailand-tour:chiangmai-treehouse.mdx.b6add966.async.js"
+  },
+  {
+    "revision": "9a507aa45b608a3dd4f2",
+    "url": "/home/p__docs__tour__thailand-tour:koh-samui-bb.mdx.f4a41c2a.async.js"
+  },
+  {
+    "revision": "16d63b756534185d8a29",
+    "url": "/home/p__docs__tour__thailand-tour:kou-samui-songkran.mdx.26b567f0.async.js"
+  },
+  {
+    "revision": "e23b120d2014dc72d796",
+    "url": "/home/p__docs__tour__thailand-tour:similan-deflate.mdx.6b0f2175.async.js"
+  },
+  {
+    "revision": "2ad298ffa4cffdd8220e",
+    "url": "/home/p__docs__tour__thailand-tour:similan-great-escape.mdx.4948a466.async.js"
+  },
+  {
+    "revision": "22f7972069dd269eb055",
+    "url": "/home/p__docs__typescript__Public,-private,-and-protected-modifiers.mdx.81aa7666.async.js"
+  },
+  {
+    "revision": "6c4c329fa2de6b27d64c",
+    "url": "/home/p__docs__typescript__Types.mdx.3abc9018.async.js"
+  },
+  {
+    "revision": "39fe2626ed1063532f1a",
+    "url": "/home/p__docs__video__cinematic.mdx.9a7bc307.async.js"
+  },
+  {
+    "revision": "3c632ca08c8a1056259b",
+    "url": "/home/p__docs__video__color-grading.mdx.7de7f573.async.js"
+  },
+  {
+    "revision": "e0469ec858f16657380d",
+    "url": "/home/p__docs__video__composition.mdx.4e4cc9c1.async.js"
+  },
+  {
+    "revision": "5ab8b8213ea2ff932760",
+    "url": "/home/p__docs__video__edting.mdx.c966cf98.async.js"
+  },
+  {
+    "revision": "e0bec146d3a3a6bfbb36",
+    "url": "/home/p__docs__video__ffmpeg.mdx.c824d7ca.async.js"
+  },
+  {
+    "revision": "8edc8d27773fd9b554a4",
+    "url": "/home/p__docs__video__gopro.mdx.bd482f04.async.js"
+  },
+  {
+    "revision": "11bc2d53fc6d5d62bdd7",
+    "url": "/home/p__index.0a905c3d.async.js"
+  },
+  {
+    "revision": "9f24f07cde8198c769a5",
+    "url": "/home/umi.a7d66906.css"
+  },
+  {
+    "revision": "9f24f07cde8198c769a5",
+    "url": "/home/umi.06e3fcd5.js"
+  },
+  {
+    "revision": "e7dffb518ae638476d8879b45b9a97a2",
+    "url": "/home/static/b737-pubket-airport.e7dffb51.jpg"
+  },
+  {
+    "revision": "a403d602b044a710e7fe33e1c0a60f82",
+    "url": "/home/static/tree-house-girl.a403d602.jpg"
+  },
+  {
+    "revision": "e84a16e467280769b747f8846de21ca2",
+    "url": "/home/static/The.Beach.2000.e84a16e4.png"
+  },
+  {
+    "revision": "3485218e03dd97f36140da20cce37c1d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c7b85c5f71465b6f0eedb39be2f26d27",
+    "url": "/home/manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/home/pwacompat.min.js"
+  }
+];
--- home/sw.js
+++ home/sw.js
@@ -1,6 +1,4 @@
-
-
-importScripts("/home/precache-manifest.7ec24275668292c76343a76098eb2995.js", "/home/workbox-v3.6.3/workbox-sw.js");
+importScripts("/home/precache-manifest.783465f231929a50593350a0c19040f1.js", "/home/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/home/workbox-v3.6.3"});
 /* eslint-env worker */
 /* global workbox */
