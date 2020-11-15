d9dc7643a2ccf86d9cbfecb12edb1c9819020893
chiaweilee <ljw@live.jp>
2020-11-15 11:42:47 +0800

    refactor: confidential

--- src/components/confidential/index.tsx
+++ src/components/confidential/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import { Skeleton } from 'antd';
 import styles from './index.less';
 
-export default function() {
-  return (
-    <Skeleton
-      className={styles.confidential}
-      loading
-      paragraph={{ rows: 1, width: '100%' }}
-      title={{ width: '80%' }}
-    />
-  );
+export default function(props: any) {
+  return <span className={styles.confidential}>{props.children}</span>;
 }
