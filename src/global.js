c0c1f3963bbcd2dea8e73471c2bbd1db2d8ec5bc
chiaweilee <ljw@live.jp>
2020-12-27 14:03:55 +0800

    Revert "feat: baidu hm"
    This reverts commit 87aef7f6

--- src/global.js
+++
@@ -1,6 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  const hm = document.createElement('script');
-  hm.src = 'https://hm.baidu.com/hm.js?2d87cebe7ebdbb176e1fb24df6ad7360';
-  const s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(hm, s);
-}
