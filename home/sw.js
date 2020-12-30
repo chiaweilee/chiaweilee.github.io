b506ac977017e9b60e02bb52bdd39c7b221d2234
chiaweilee <ljw@live.jp>
2020-12-30 01:33:08 +0000

    build

---
+++ home/sw.js
@@ -0,0 +1,8 @@
+importScripts("/home/precache-manifest.3529668871694325a687329a48825a9c.js", "/home/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/home/workbox-v3.6.3"});
+/* eslint-env worker */
+/* global workbox */
+
+workbox.routing.registerRoute(/.*\.html$/, workbox.strategies.networkFirst());
+workbox.routing.registerRoute(/.*.(?:js|css)/, workbox.strategies.cacheFirst());
+
