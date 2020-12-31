0936a870772739276e39dbccd249fbaea24ecfbe
+v <ljw@live.jp>
2020-12-31 22:05:47 +0800

    Update index.tsx

--- src/components/confidential/index.tsx
+++ src/components/confidential/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './index.less';
 
 export default function(props: any) {
-  return <span className={styles.confidential} {...props} />;
+  return <span className={styles.confidential} onClick={props.onClick}>{props.children}</span>;
 }
