43f6390174f145d4d33dea96835513537d028221
+v <ljw@live.jp>
2020-12-31 18:17:43 +0800

    Create index.tsx

---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+import Map from '@/components/map';
+import Cryptor from '@/components/cryptor';
+import HotelNights from '@/components/hotel-nights';
+
+export default {
+  Map,
+  Cryptor,
+  HotelNights,
+}
