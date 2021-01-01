143d120f3f70382e0d883d46db526193b6d6e382
chiaweilee <ljw@live.jp>
2021-01-01 06:06:38 +0000

    build

---
+++ home/precache-manifest.234170bc2718573287dda792132ff39b.js
@@ -0,0 +1,438 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e8948b0a8e73c17c53f",
+    "url": "/home/vendors.31b6d974.chunk.css"
+  },
+  {
+    "revision": "8e8948b0a8e73c17c53f",
+    "url": "/home/vendors.12716d57.async.js"
+  },
+  {
+    "revision": "4c3827eb4ca6ca967149",
+    "url": "/home/p__docs___layout.94b09606.async.js"
+  },
+  {
+    "revision": "68080bac14652799a414",
+    "url": "/home/p__docs__english__pronunciation.mdx.b7e08b15.chunk.css"
+  },
+  {
+    "revision": "68080bac14652799a414",
+    "url": "/home/p__docs__english__pronunciation.mdx.bf624008.async.js"
+  },
+  {
+    "revision": "cb1c19b78a36c6eb4b40",
+    "url": "/home/p__docs__f2e__css__BFC.mdx.48445f3b.async.js"
+  },
+  {
+    "revision": "58d4e957fec1efd95a3c",
+    "url": "/home/p__docs__f2e__css__Box-model.mdx.98b354c4.async.js"
+  },
+  {
+    "revision": "ddb6f73900afc14a5dd6",
+    "url": "/home/p__docs__f2e__css__Horizontal-and-Vertical-Center.mdx.9f08022e.async.js"
+  },
+  {
+    "revision": "418b98d0932a9b001a1d",
+    "url": "/home/p__docs__f2e__css__REM.mdx.171c78f3.async.js"
+  },
+  {
+    "revision": "169d7ac1459ce7421c3f",
+    "url": "/home/p__docs__f2e__http__Headers-and-Caching.mdx.0b414910.async.js"
+  },
+  {
+    "revision": "cf28c403293c0e2f7373",
+    "url": "/home/p__docs__f2e__javascript__AMD-UMD-CommonJS.mdx.1249833c.async.js"
+  },
+  {
+    "revision": "d8881126dc298e4b8cdd",
+    "url": "/home/p__docs__f2e__javascript__Array.mdx.98e924de.async.js"
+  },
+  {
+    "revision": "67b5fb2372f58aa02c1a",
+    "url": "/home/p__docs__f2e__javascript__BigInt.mdx.3ad7f1fd.async.js"
+  },
+  {
+    "revision": "f080a9e1f044081ad153",
+    "url": "/home/p__docs__f2e__javascript__Closure.mdx.2be9e5a0.async.js"
+  },
+  {
+    "revision": "fb59a7678490ed67a38b",
+    "url": "/home/p__docs__f2e__javascript__Deep&ShallowCopy.mdx.e4df86df.async.js"
+  },
+  {
+    "revision": "04e1e82ceea7d8fef4cb",
+    "url": "/home/p__docs__f2e__javascript__Enumerate-an-object.mdx.47376c4f.async.js"
+  },
+  {
+    "revision": "2af3a2d0b2c4bba09b81",
+    "url": "/home/p__docs__f2e__javascript__Event.mdx.6fc13453.async.js"
+  },
+  {
+    "revision": "75ff7f897228a7a9567d",
+    "url": "/home/p__docs__f2e__javascript__Microtasks&Macrotasks.mdx.7e26594e.async.js"
+  },
+  {
+    "revision": "a292a53935a5f02235d2",
+    "url": "/home/p__docs__f2e__javascript__Promise.mdx.82853e6d.async.js"
+  },
+  {
+    "revision": "9cefad255ed66e06a9ba",
+    "url": "/home/p__docs__f2e__javascript__Proxy.mdx.6446b90b.async.js"
+  },
+  {
+    "revision": "bb008846af0dfa01c9f9",
+    "url": "/home/p__docs__f2e__javascript__Set.mdx.36133dd3.async.js"
+  },
+  {
+    "revision": "e495b0b04fe35e9cd5d3",
+    "url": "/home/p__docs__f2e__javascript__String.mdx.6cd292f8.async.js"
+  },
+  {
+    "revision": "370e4c380d758c0ff7f3",
+    "url": "/home/p__docs__f2e__javascript__XSS.mdx.d63c45a5.async.js"
+  },
+  {
+    "revision": "94179d2c8c81ff17c571",
+    "url": "/home/p__docs__f2e__javascript__classes.mdx.6ae48009.async.js"
+  },
+  {
+    "revision": "359d98af9fe235999fd5",
+    "url": "/home/p__docs__f2e__javascript__falsy-truthy.mdx.ab59f93d.async.js"
+  },
+  {
+    "revision": "06c96ebe273f9e0f21be",
+    "url": "/home/p__docs__f2e__javascript__memory-leak.mdx.d8452576.async.js"
+  },
+  {
+    "revision": "bbf0fbd1d0a899941fce",
+    "url": "/home/p__docs__f2e__javascript__null.mdx.b95de94e.async.js"
+  },
+  {
+    "revision": "e9413740b25f0a085fb6",
+    "url": "/home/p__docs__f2e__javascript__requestAnimationFrame.mdx.5c50ff9e.async.js"
+  },
+  {
+    "revision": "f451cd0332392afebb85",
+    "url": "/home/p__docs__f2e__javascript__require&import.mdx.05aecc64.async.js"
+  },
+  {
+    "revision": "a0ba910fd0f81a834fcd",
+    "url": "/home/p__docs__f2e__nodejs__npm.mdx.0304b51c.async.js"
+  },
+  {
+    "revision": "6dfe33af6353883cedf1",
+    "url": "/home/p__docs__f2e__nodejs__stdout.mdx.8a041e12.async.js"
+  },
+  {
+    "revision": "a55f3a60c77e9bbedf9c",
+    "url": "/home/p__docs__f2e__pwa__manifest.mdx.bd10aafc.async.js"
+  },
+  {
+    "revision": "f96c94e0435a920f96a6",
+    "url": "/home/p__docs__f2e__vuejs__data-function.mdx.fff227f6.async.js"
+  },
+  {
+    "revision": "e6c8c89e1a99bf73be67",
+    "url": "/home/p__docs__f2e__vuejs__memory-leak.mdx.8c69f674.async.js"
+  },
+  {
+    "revision": "45662ea2470341d17903",
+    "url": "/home/p__docs__f2e__vuejs__nextTick.mdx.a67b06ab.async.js"
+  },
+  {
+    "revision": "9eca05b7d0a794830ea6",
+    "url": "/home/p__docs__f2e__vuejs__v-if&v-for.mdx.088c9cb1.async.js"
+  },
+  {
+    "revision": "c2d0806b25d889992707",
+    "url": "/home/p__docs__f2e__webpack__Plugins.mdx.47a90a0d.async.js"
+  },
+  {
+    "revision": "c2caf73397702e0c6e62",
+    "url": "/home/p__docs__food__hotpot.mdx.f6e56ff8.async.js"
+  },
+  {
+    "revision": "526002d68a5a52dcd286",
+    "url": "/home/p__docs__index.mdx.3065edad.async.js"
+  },
+  {
+    "revision": "11e379d013edad7d390a",
+    "url": "/home/p__docs__movie__TDK.mdx.61c3b43f.async.js"
+  },
+  {
+    "revision": "2222a5736592331b471f",
+    "url": "/home/p__docs__movie__lines.mdx.b29a3f88.async.js"
+  },
+  {
+    "revision": "8ad0b45bdc07171c80b4",
+    "url": "/home/p__docs__t7__0-preface.mdx.e47a936b.async.js"
+  },
+  {
+    "revision": "a257e28becae301930c0",
+    "url": "/home/p__docs__t7__1-tour.mdx.411682a0.async.js"
+  },
+  {
+    "revision": "5b4bd1e0037df6a3705f",
+    "url": "/home/p__docs__t7__2-tour.mdx.a8d72aa9.async.js"
+  },
+  {
+    "revision": "09b0a7235fd968f0ec13",
+    "url": "/home/p__docs__t7__3-tour.mdx.29daf073.async.js"
+  },
+  {
+    "revision": "ab15d76e21d957365130",
+    "url": "/home/p__docs__t7__4-tour.mdx.3ca7a051.async.js"
+  },
+  {
+    "revision": "259531aa09aea499f82e",
+    "url": "/home/p__docs__t7__5-tour.mdx.27adb486.async.js"
+  },
+  {
+    "revision": "fa655b4413dc40edd7d7",
+    "url": "/home/p__docs__t7__6-tour.mdx.da960d84.async.js"
+  },
+  {
+    "revision": "8d0bbdfe4a2ab21a2bb9",
+    "url": "/home/p__docs__t7__7-tour.mdx.df12d471.async.js"
+  },
+  {
+    "revision": "19d22d87df601d69f83b",
+    "url": "/home/p__docs__tour__1st-thailand-tour:phuket-catch-the-wind.mdx.f0abc1e3.async.js"
+  },
+  {
+    "revision": "e6d662b1a1a04838affb",
+    "url": "/home/p__docs__tour__1st-thailand-tour:sukhothai-riding-in-dark.mdx.81bdad7e.async.js"
+  },
+  {
+    "revision": "3fca4f48c91dd1ff00ca",
+    "url": "/home/p__docs__tour__1st-thailand-tour:sukhothai-unexpected-ride.mdx.e1a6feb1.async.js"
+  },
+  {
+    "revision": "32201a82672a44a7f6c5",
+    "url": "/home/p__docs__tour__2nd-malaysia:mabul-scuba-paradise.mdx.2d7c5b96.async.js"
+  },
+  {
+    "revision": "2a17c2587e580a5271da",
+    "url": "/home/p__docs__tour__checklist.mdx.8fbec1f7.async.js"
+  },
+  {
+    "revision": "eeaa50293692fd96d8d9",
+    "url": "/home/p__docs__tour__emei--20℃.mdx.810abd8e.async.js"
+  },
+  {
+    "revision": "116d472f0ab13763aa82",
+    "url": "/home/p__docs__tour__malaysia-tour:Mount Kinabalu.mdx.0830ce99.async.js"
+  },
+  {
+    "revision": "bf67a45db35c9c4c8d32",
+    "url": "/home/p__docs__tour__thailand-tour:chiangmai-treehouse.mdx.d3d40347.async.js"
+  },
+  {
+    "revision": "51d98496ed6de12b7ae9",
+    "url": "/home/p__docs__tour__thailand-tour:koh-samui-bb.mdx.3baeb021.async.js"
+  },
+  {
+    "revision": "918ff6ba438c751b1b09",
+    "url": "/home/p__docs__tour__thailand-tour:kou-samui-songkran.mdx.ee3154fd.async.js"
+  },
+  {
+    "revision": "e4d62abf5c5b0788e118",
+    "url": "/home/p__docs__tour__thailand-tour:similan-deflate.mdx.eda9597e.async.js"
+  },
+  {
+    "revision": "075126ffb255a5ba4d05",
+    "url": "/home/p__docs__tour__thailand-tour:similan-great-escape.mdx.16ed9071.async.js"
+  },
+  {
+    "revision": "86380a56865c336cb7ec",
+    "url": "/home/p__docs__typescript__Public,-private,-and-protected-modifiers.mdx.cec2b3ae.async.js"
+  },
+  {
+    "revision": "ff3963d84b96b0822e7a",
+    "url": "/home/p__docs__typescript__Types.mdx.bc90910f.async.js"
+  },
+  {
+    "revision": "cb6312ae0cb9af534ae7",
+    "url": "/home/p__docs__util__encoder.mdx.8a1a993c.async.js"
+  },
+  {
+    "revision": "0cecf9e209a015d0288e",
+    "url": "/home/p__docs__video__cinematic.mdx.2e6f106d.async.js"
+  },
+  {
+    "revision": "99648c0fe8b6674172ee",
+    "url": "/home/p__docs__video__color-grading.mdx.1c9061e9.async.js"
+  },
+  {
+    "revision": "72a7474f0798a12c092a",
+    "url": "/home/p__docs__video__composition.mdx.3258ac92.async.js"
+  },
+  {
+    "revision": "91424184c457ea7a92b3",
+    "url": "/home/p__docs__video__edting.mdx.c8662cb1.async.js"
+  },
+  {
+    "revision": "b85831b3d508b57ae6d8",
+    "url": "/home/p__docs__video__ffmpeg.mdx.e5f6e44b.async.js"
+  },
+  {
+    "revision": "8b2663fa68b5b44c2eea",
+    "url": "/home/p__docs__video__gopro.mdx.6815a961.async.js"
+  },
+  {
+    "revision": "a171ae90e5ae303d3998",
+    "url": "/home/p__index.9800a427.async.js"
+  },
+  {
+    "revision": "b5404cb22660bf0135f3",
+    "url": "/home/umi.a9bfe3ff.css"
+  },
+  {
+    "revision": "b5404cb22660bf0135f3",
+    "url": "/home/umi.60e86418.js"
+  },
+  {
+    "revision": "2a6127e4d08271ca15420a15fce5b40a",
+    "url": "/home/static/treehouse-2.2a6127e4.jpg"
+  },
+  {
+    "revision": "b4166e6891677abd3118e8289b185d5d",
+    "url": "/home/static/fullmoon-1.b4166e68.jpg"
+  },
+  {
+    "revision": "e84a16e467280769b747f8846de21ca2",
+    "url": "/home/static/beach.e84a16e4.png"
+  },
+  {
+    "revision": "3490762ff28016460056a7bda9311d93",
+    "url": "/home/static/moondance.3490762f.jpg"
+  },
+  {
+    "revision": "2b8f131ce68afe06a7931db1b2dee4a0",
+    "url": "/home/static/treehouse-1.2b8f131c.jpg"
+  },
+  {
+    "revision": "e7dffb518ae638476d8879b45b9a97a2",
+    "url": "/home/static/b737-pubket-airport.e7dffb51.jpg"
+  },
+  {
+    "revision": "7ca817d26e9fb4348ca9b03d7ed7935e",
+    "url": "/home/static/vicky-fish.7ca817d2.png"
+  },
+  {
+    "revision": "d26c38453458dc87c16ee411bbf883ab",
+    "url": "/home/static/mabul-1.d26c3845.jpg"
+  },
+  {
+    "revision": "b8c5da45f21e815b10fc8fbebf048e0a",
+    "url": "/home/static/Career.b8c5da45.ttf"
+  },
+  {
+    "revision": "a403d602b044a710e7fe33e1c0a60f82",
+    "url": "/home/static/tree-house-girl.a403d602.jpg"
+  },
+  {
+    "revision": "65cd94f1c00fc93664e03ef1dbf456bb",
+    "url": "/home/static/mabul-2.65cd94f1.jpg"
+  },
+  {
+    "revision": "7833dc5c9001507200de40d8f92a1687",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e84a16e467280769b747f8846de21ca2",
+    "url": "/home/assets/beach.png"
+  },
+  {
+    "revision": "b4166e6891677abd3118e8289b185d5d",
+    "url": "/home/assets/fullmoon-1.jpg"
+  },
+  {
+    "revision": "d26c38453458dc87c16ee411bbf883ab",
+    "url": "/home/assets/mabul-1.jpg"
+  },
+  {
+    "revision": "65cd94f1c00fc93664e03ef1dbf456bb",
+    "url": "/home/assets/mabul-2.jpg"
+  },
+  {
+    "revision": "3490762ff28016460056a7bda9311d93",
+    "url": "/home/assets/moondance.jpg"
+  },
+  {
+    "revision": "2b8f131ce68afe06a7931db1b2dee4a0",
+    "url": "/home/assets/treehouse-1.jpg"
+  },
+  {
+    "revision": "2a6127e4d08271ca15420a15fce5b40a",
+    "url": "/home/assets/treehouse-2.jpg"
+  },
+  {
+    "revision": "7ca817d26e9fb4348ca9b03d7ed7935e",
+    "url": "/home/assets/vicky-fish.png"
+  },
+  {
+    "revision": "d9b51a38aee008811822ac5ddd618a38",
+    "url": "/home/assets/gt-2.jpg"
+  },
+  {
+    "revision": "2bd32caa31baa9ddac4ad8eb68c498d8",
+    "url": "/home/assets/latu.jpg"
+  },
+  {
+    "revision": "3381074177bc97d3daac86ea18b2c85c",
+    "url": "/home/assets/sara.jpg"
+  },
+  {
+    "revision": "1275b7c5d17e872da0fa3469bbe21127",
+    "url": "/home/assets/cry.jpg"
+  },
+  {
+    "revision": "8a08e3526b9dc02c1d926f63592af448",
+    "url": "/home/assets/snake.jpg"
+  },
+  {
+    "revision": "19541abd0c73e5e4bb56f2459315079a",
+    "url": "/home/assets/qin.jpg"
+  },
+  {
+    "revision": "d2536da10dce8f5ae62993e34e2179ce",
+    "url": "/home/assets/gt-1.JPG"
+  },
+  {
+    "revision": "6992825162bf8b80d1f5397886d3ada0",
+    "url": "/home/assets/similan.jpg"
+  },
+  {
+    "revision": "86def96f95ef7421062fcf10f36956b1",
+    "url": "/home/assets/mn-1.JPG"
+  },
+  {
+    "revision": "4cc1ce0d2814aec334fb3c575ff8e9d2",
+    "url": "/home/assets/treehouse-all.jpg"
+  },
+  {
+    "revision": "b710c4aa2934043c5f56fcf4b1e53c09",
+    "url": "/home/assets/mn-2.JPG"
+  },
+  {
+    "revision": "c731b16741677f44b313178a3cc34886",
+    "url": "/home/assets/klcc.jpg"
+  },
+  {
+    "revision": "1e0cbe8200536e8af81b5099d4418058",
+    "url": "/home/assets/t.jpg"
+  },
+  {
+    "revision": "7d64d0308e2cad3c249a95ce90c6330a",
+    "url": "/home/assets/bkk.jpg"
+  },
+  {
+    "revision": "c7b85c5f71465b6f0eedb39be2f26d27",
+    "url": "/home/manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/home/pwacompat.min.js"
+  }
+];
--- home/sw.js
+++ home/sw.js
@@ -1,4 +1,4 @@
-importScripts("/home/precache-manifest.bfd6e016a5f5ad581af02dbb55d8c0b3.js", "/home/workbox-v3.6.3/workbox-sw.js");
+importScripts("/home/precache-manifest.234170bc2718573287dda792132ff39b.js", "/home/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/home/workbox-v3.6.3"});
 /* eslint-env worker */
 /* global workbox */
