e08a336606681f3aaa5429ddd7a75127dff32e50
+v <ljw@live.jp>
2021-01-01 15:42:55 +0800

    Update index.tsx

--- src/components/img/index.tsx
+++ src/components/img/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function({ src }) {
-  return <img src={`../assets/${src}`} alt="" />;
+  return <img src={process.env.NODE_ENV === 'development' ? `../assets/${src}` : `/home/assets/${src}`} alt="" />;
 }
