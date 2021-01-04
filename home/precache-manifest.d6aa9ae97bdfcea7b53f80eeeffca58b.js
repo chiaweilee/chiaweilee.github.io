88b2636a8b6ab945e4432986e14ab68315eb1cb0
chiaweilee <ljw@live.jp>
2021-01-04 01:04:16 +0000

    build

--- home/precache-manifest.d6aa9ae97bdfcea7b53f80eeeffca58b.js
+++ home/precache-manifest.d6aa9ae97bdfcea7b53f80eeeffca58b.js
@@ -316,12 +316,12 @@ self.__precacheManifest = [
     "url": "/home/index.html"
   },
   {
-    "revision": "2bd32caa31baa9ddac4ad8eb68c498d8",
-    "url": "/home/assets/latu.jpg"
-  },
-  {
     "revision": "d9b51a38aee008811822ac5ddd618a38",
     "url": "/home/assets/gt-2.jpg"
+  },
+  {
+    "revision": "2bd32caa31baa9ddac4ad8eb68c498d8",
+    "url": "/home/assets/latu.jpg"
   },
   {
     "revision": "3490762ff28016460056a7bda9311d93",
@@ -376,12 +376,12 @@ self.__precacheManifest = [
     "url": "/home/assets/mn-2.JPG"
   },
   {
-    "revision": "86def96f95ef7421062fcf10f36956b1",
-    "url": "/home/assets/mn-1.JPG"
-  },
-  {
     "revision": "4cc1ce0d2814aec334fb3c575ff8e9d2",
     "url": "/home/assets/treehouse-all.jpg"
+  },
+  {
+    "revision": "86def96f95ef7421062fcf10f36956b1",
+    "url": "/home/assets/mn-1.JPG"
   },
   {
     "revision": "c731b16741677f44b313178a3cc34886",
