cb7defaa0738bbfc72b972ace3b29c1b0cd30d9d
chiaweilee <ljw@live.jp>
2021-01-06 11:02:37 +0000

    build

--- home/precache-manifest.d5227e8f2cc78e90d21fff20a33ce7f1.js
+++ home/precache-manifest.d5227e8f2cc78e90d21fff20a33ce7f1.js
@@ -320,12 +320,12 @@ self.__precacheManifest = [
     "url": "/home/index.html"
   },
   {
-    "revision": "d9b51a38aee008811822ac5ddd618a38",
-    "url": "/home/assets/gt-2.jpg"
-  },
-  {
     "revision": "2bd32caa31baa9ddac4ad8eb68c498d8",
     "url": "/home/assets/latu.jpg"
+  },
+  {
+    "revision": "d9b51a38aee008811822ac5ddd618a38",
+    "url": "/home/assets/gt-2.jpg"
   },
   {
     "revision": "3490762ff28016460056a7bda9311d93",
@@ -356,12 +356,12 @@ self.__precacheManifest = [
     "url": "/home/assets/snake.jpg"
   },
   {
-    "revision": "19541abd0c73e5e4bb56f2459315079a",
-    "url": "/home/assets/qin.jpg"
-  },
-  {
     "revision": "b4166e6891677abd3118e8289b185d5d",
     "url": "/home/assets/fullmoon-1.jpg"
+  },
+  {
+    "revision": "19541abd0c73e5e4bb56f2459315079a",
+    "url": "/home/assets/qin.jpg"
   },
   {
     "revision": "d2536da10dce8f5ae62993e34e2179ce",
@@ -376,16 +376,16 @@ self.__precacheManifest = [
     "url": "/home/assets/similan.jpg"
   },
   {
-    "revision": "b710c4aa2934043c5f56fcf4b1e53c09",
-    "url": "/home/assets/mn-2.JPG"
-  },
-  {
     "revision": "86def96f95ef7421062fcf10f36956b1",
     "url": "/home/assets/mn-1.JPG"
   },
   {
     "revision": "4cc1ce0d2814aec334fb3c575ff8e9d2",
     "url": "/home/assets/treehouse-all.jpg"
+  },
+  {
+    "revision": "b710c4aa2934043c5f56fcf4b1e53c09",
+    "url": "/home/assets/mn-2.JPG"
   },
   {
     "revision": "c731b16741677f44b313178a3cc34886",
--- home/sw.js
+++ home/sw.js
@@ -1,4 +1,4 @@
-importScripts("/home/precache-manifest.bbd63acd20eb10ad894b6f7257b90661.js", "/home/workbox-v3.6.3/workbox-sw.js");
+importScripts("/home/precache-manifest.d5227e8f2cc78e90d21fff20a33ce7f1.js", "/home/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/home/workbox-v3.6.3"});
 /* eslint-env worker */
 /* global workbox */
