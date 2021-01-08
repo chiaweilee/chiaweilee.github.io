294da2a1a4582d6c22561627ddeaa34bc7f82c58
chiaweilee <ljw@live.jp>
2021-01-08 17:07:15 +0000

    build

--- home/precache-manifest.da4a62340e5362135be8239e74c628d4.js
+++ home/precache-manifest.da4a62340e5362135be8239e74c628d4.js
@@ -332,32 +332,32 @@ self.__precacheManifest = [
     "url": "/home/assets/moondance.jpg"
   },
   {
-    "revision": "faddd726fae6b85a013ac86bc7c1df99",
-    "url": "/home/assets/mob.jpg.json"
-  },
-  {
     "revision": "3381074177bc97d3daac86ea18b2c85c",
     "url": "/home/assets/sara.jpg"
+  },
+  {
+    "revision": "faddd726fae6b85a013ac86bc7c1df99",
+    "url": "/home/assets/mob.jpg.json"
   },
   {
     "revision": "d26c38453458dc87c16ee411bbf883ab",
     "url": "/home/assets/mabul-1.jpg"
   },
   {
-    "revision": "e84a16e467280769b747f8846de21ca2",
-    "url": "/home/assets/beach.png"
-  },
-  {
     "revision": "1275b7c5d17e872da0fa3469bbe21127",
     "url": "/home/assets/cry.jpg"
   },
   {
-    "revision": "65cd94f1c00fc93664e03ef1dbf456bb",
-    "url": "/home/assets/mabul-2.jpg"
+    "revision": "e84a16e467280769b747f8846de21ca2",
+    "url": "/home/assets/beach.png"
   },
   {
     "revision": "8a08e3526b9dc02c1d926f63592af448",
     "url": "/home/assets/snake.jpg"
+  },
+  {
+    "revision": "65cd94f1c00fc93664e03ef1dbf456bb",
+    "url": "/home/assets/mabul-2.jpg"
   },
   {
     "revision": "b4166e6891677abd3118e8289b185d5d",
@@ -404,12 +404,12 @@ self.__precacheManifest = [
     "url": "/home/assets/t.jpg"
   },
   {
-    "revision": "ffc0041a200c906f93b91906be325df6",
-    "url": "/home/assets/IMG_20170413_234141.jpg.json"
-  },
-  {
     "revision": "7d64d0308e2cad3c249a95ce90c6330a",
     "url": "/home/assets/bkk.jpg"
+  },
+  {
+    "revision": "ffc0041a200c906f93b91906be325df6",
+    "url": "/home/assets/IMG_20170413_234141.jpg.json"
   },
   {
     "revision": "2a6127e4d08271ca15420a15fce5b40a",
--- home/sw.js
+++ home/sw.js
@@ -1,4 +1,4 @@
-importScripts("/home/precache-manifest.fc7519c526dd6ab1606711a0009a9659.js", "/home/workbox-v3.6.3/workbox-sw.js");
+importScripts("/home/precache-manifest.da4a62340e5362135be8239e74c628d4.js", "/home/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/home/workbox-v3.6.3"});
 /* eslint-env worker */
 /* global workbox */
