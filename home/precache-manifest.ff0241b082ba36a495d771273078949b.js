f538b8c157540a99951ef55c9dcb95f602747381
chiaweilee <ljw@live.jp>
2021-01-08 19:30:17 +0000

    build

--- home/precache-manifest.ff0241b082ba36a495d771273078949b.js
+++ home/precache-manifest.ff0241b082ba36a495d771273078949b.js
@@ -380,10 +380,6 @@ self.__precacheManifest = [
     "url": "/home/assets/similan.jpg"
   },
   {
-    "revision": "86def96f95ef7421062fcf10f36956b1",
-    "url": "/home/assets/mn-1.JPG"
-  },
-  {
     "revision": "b710c4aa2934043c5f56fcf4b1e53c09",
     "url": "/home/assets/mn-2.JPG"
   },
@@ -392,16 +388,20 @@ self.__precacheManifest = [
     "url": "/home/assets/treehouse-all.jpg"
   },
   {
+    "revision": "86def96f95ef7421062fcf10f36956b1",
+    "url": "/home/assets/mn-1.JPG"
+  },
+  {
     "revision": "c731b16741677f44b313178a3cc34886",
     "url": "/home/assets/klcc.jpg"
   },
   {
-    "revision": "8341c6934442df17460159b03efdbc98",
-    "url": "/home/assets/kohtao1.jpg.json"
-  },
-  {
     "revision": "1e0cbe8200536e8af81b5099d4418058",
     "url": "/home/assets/t.jpg"
+  },
+  {
+    "revision": "8341c6934442df17460159b03efdbc98",
+    "url": "/home/assets/kohtao1.jpg.json"
   },
   {
     "revision": "ffc0041a200c906f93b91906be325df6",
