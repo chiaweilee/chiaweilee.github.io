41d7708f316d9413e43cea50140a491a98945573
chiaweilee <ljw@live.jp>
2021-01-08 14:14:55 +0000

    build

--- home/precache-manifest.3cd791ba15fcf71652020363f5afd272.js
+++ home/precache-manifest.3cd791ba15fcf71652020363f5afd272.js
@@ -332,24 +332,24 @@ self.__precacheManifest = [
     "url": "/home/assets/moondance.jpg"
   },
   {
-    "revision": "faddd726fae6b85a013ac86bc7c1df99",
-    "url": "/home/assets/mob.jpg.json"
-  },
-  {
     "revision": "3381074177bc97d3daac86ea18b2c85c",
     "url": "/home/assets/sara.jpg"
+  },
+  {
+    "revision": "faddd726fae6b85a013ac86bc7c1df99",
+    "url": "/home/assets/mob.jpg.json"
   },
   {
     "revision": "d26c38453458dc87c16ee411bbf883ab",
     "url": "/home/assets/mabul-1.jpg"
   },
   {
-    "revision": "1275b7c5d17e872da0fa3469bbe21127",
-    "url": "/home/assets/cry.jpg"
-  },
-  {
     "revision": "e84a16e467280769b747f8846de21ca2",
     "url": "/home/assets/beach.png"
+  },
+  {
+    "revision": "1275b7c5d17e872da0fa3469bbe21127",
+    "url": "/home/assets/cry.jpg"
   },
   {
     "revision": "65cd94f1c00fc93664e03ef1dbf456bb",
@@ -380,6 +380,10 @@ self.__precacheManifest = [
     "url": "/home/assets/similan.jpg"
   },
   {
+    "revision": "4cc1ce0d2814aec334fb3c575ff8e9d2",
+    "url": "/home/assets/treehouse-all.jpg"
+  },
+  {
     "revision": "86def96f95ef7421062fcf10f36956b1",
     "url": "/home/assets/mn-1.JPG"
   },
@@ -388,16 +392,12 @@ self.__precacheManifest = [
     "url": "/home/assets/mn-2.JPG"
   },
   {
-    "revision": "4cc1ce0d2814aec334fb3c575ff8e9d2",
-    "url": "/home/assets/treehouse-all.jpg"
+    "revision": "8341c6934442df17460159b03efdbc98",
+    "url": "/home/assets/kohtao1.jpg.json"
   },
   {
     "revision": "c731b16741677f44b313178a3cc34886",
     "url": "/home/assets/klcc.jpg"
-  },
-  {
-    "revision": "8341c6934442df17460159b03efdbc98",
-    "url": "/home/assets/kohtao1.jpg.json"
   },
   {
     "revision": "1e0cbe8200536e8af81b5099d4418058",
