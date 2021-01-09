1ced044f69aae202317a400072c96bc02678626b
chiaweilee <ljw@live.jp>
2021-01-09 10:01:08 +0000

    build

--- home/sw.js
+++ home/sw.js
@@ -1,4 +1,4 @@
-importScripts("/home/precache-manifest.2462f8d76bb2fdd2e99ee18add69d6a4.js", "/home/workbox-v3.6.3/workbox-sw.js");
+importScripts("/home/precache-manifest.a9d08cc83e17b0a664f5457a7202fdbf.js", "/home/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/home/workbox-v3.6.3"});
 /* eslint-env worker */
 /* global workbox */
