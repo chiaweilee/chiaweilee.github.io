e42f49c736ce9f4880bab9f3b871d4afddde7685
+v <ljw@live.jp>
2021-01-09 16:50:51 +0800

    Update global.tsx

--- src/global.tsx
+++ src/global.tsx
@@ -1,5 +1,5 @@
 import { Toast } from 'antd-mobile';
 
-window.onerror = (...argv) => {
-  Toast.offline(argv.join(' '), process.env.NODE_ENV === 'development' ? 1 : 10);
+window.onerror = msg => {
+  Toast.offline(msg, process.env.NODE_ENV === 'development' ? 1 : 10);
 };
