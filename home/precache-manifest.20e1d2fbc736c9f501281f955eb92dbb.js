3c2c97a10bdbd42eba23bf4e36aa946623864101
chiaweilee <ljw@live.jp>
2021-01-09 02:14:54 +0000

    build

--- home/precache-manifest.20e1d2fbc736c9f501281f955eb92dbb.js
+++ home/precache-manifest.20e1d2fbc736c9f501281f955eb92dbb.js
@@ -344,12 +344,12 @@ self.__precacheManifest = [
     "url": "/home/assets/mabul-1.jpg"
   },
   {
-    "revision": "1275b7c5d17e872da0fa3469bbe21127",
-    "url": "/home/assets/cry.jpg"
-  },
-  {
     "revision": "e84a16e467280769b747f8846de21ca2",
     "url": "/home/assets/beach.png"
+  },
+  {
+    "revision": "1275b7c5d17e872da0fa3469bbe21127",
+    "url": "/home/assets/cry.jpg"
   },
   {
     "revision": "65cd94f1c00fc93664e03ef1dbf456bb",
@@ -392,12 +392,12 @@ self.__precacheManifest = [
     "url": "/home/assets/treehouse-all.jpg"
   },
   {
-    "revision": "8341c6934442df17460159b03efdbc98",
-    "url": "/home/assets/kohtao1.jpg.json"
-  },
-  {
     "revision": "c731b16741677f44b313178a3cc34886",
     "url": "/home/assets/klcc.jpg"
+  },
+  {
+    "revision": "8341c6934442df17460159b03efdbc98",
+    "url": "/home/assets/kohtao1.jpg.json"
   },
   {
     "revision": "1e0cbe8200536e8af81b5099d4418058",
--- home/sw.js
+++ home/sw.js
@@ -1,4 +1,4 @@
-importScripts("/home/precache-manifest.0fc7739601a267a56bf6454ec41dcada.js", "/home/workbox-v3.6.3/workbox-sw.js");
+importScripts("/home/precache-manifest.20e1d2fbc736c9f501281f955eb92dbb.js", "/home/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/home/workbox-v3.6.3"});
 /* eslint-env worker */
 /* global workbox */
