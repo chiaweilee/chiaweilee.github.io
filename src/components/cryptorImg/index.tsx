a36474c35184e4c1847b8096cd3031ad2a263204
chiaweilee <ljw@live.jp>
2021-01-17 12:48:23 +0800

    fix: IIF

--- src/components/cryptorImg/index.tsx
+++ src/components/cryptorImg/index.tsx
@@ -2,25 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { decoder, secretKey } from '@/components/cryptor';
 import { onTouch } from '@/utils/e';
 
-export default function(props) {
+export default function (props) {
   const [hidden, setHidden] = useState(true);
   const [img, setImg] = useState(undefined);
   const { src, strict } = props;
 
-  // @ts-ignore
-  useEffect(async () => {
-    if (localStorage.getItem(secretKey)) {
-      const res = await (
-        await fetch(
-          process.env.NODE_ENV === 'development'
-            ? `../assets/${src}.json`
-            : `/home/assets/${src}.json`,
-        )
-      ).json();
-      if (res) {
-        setImg(decoder(res));
+  useEffect(() => {
+    (async () => {
+      if (localStorage.getItem(secretKey)) {
+        const res = await (
+          await fetch(
+            process.env.NODE_ENV === 'development'
+              ? `../assets/${src}.json`
+              : `/home/assets/${src}.json`,
+          )
+        ).json();
+        if (res) {
+          setImg(decoder(res));
+        }
       }
-    }
+    })();
   }, [src]);
 
   if (!img) {
