2c70bd10f33885da1f92777ad3893a3292c50ba2
chiaweilee <ljw@live.jp>
2021-01-24 13:56:40 +0800

    remove: pwa

--- src/sw.js
+++
@@ -1,5 +0,0 @@
-/* eslint-env worker */
-/* global workbox */
-
-workbox.routing.registerRoute(/.*\.html$/, workbox.strategies.networkFirst());
-workbox.routing.registerRoute(/.*.(?:js|css)/, workbox.strategies.cacheFirst());
--- src/theme.js
+++
@@ -1,7 +0,0 @@
-// const manifest = require('./manifest');
-
-// https://github.com/ant-design/ant-design-mobile/blob/master/components/style/themes/default.less
-module.exports = {
-  // '@brand-primary': manifest.theme_color,
-  // '@brand-primary-tap': '#98dbd9',
-};
