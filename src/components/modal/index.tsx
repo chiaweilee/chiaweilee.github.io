5ac7289ec98a6f9423ccb72fad351d0436874f7c
chiaweilee <ljw@live.jp>
2021-03-22 21:45:59 +0800

    feat: prevent scroll

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Icon from '@/components/icon';
 import styles from './index.less';
 
 export default function (props: any) {
   const { children, __unmount__ } = props;
+
+  useEffect(() => {
+    document.getElementsByTagName('body')[0].style.height = '100%';
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+    return () => {
+      document.getElementsByTagName('body')[0].style.height = '';
+      document.getElementsByTagName('body')[0].style.overflow = '';
+    };
+  }, []);
 
   return (
     <div>
